a8094156bb03d294302b1ac1bb48d3535fc38e82
samuel hughes <samuelhugh33@gmail.com>
2021-11-30 20:24:50 -0600

    Adding Java Projects!

---
+++ 01-java-fundamentals/01-Java First Program/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    // Entry Point Method
+    public static void main(String[] args) {
+        System.out.println("My name is Sam");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Tulsa, OK");
+    }
+}
