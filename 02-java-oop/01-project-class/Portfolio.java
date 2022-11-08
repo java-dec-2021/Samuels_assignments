f4a4723af65450ee0049219d2287e98771342616
samuel hughes <samuelhugh33@gmail.com>
2022-11-08 11:46:09 -0600

    Refactoring

---
+++ 02-java-oop/01-project-class/Portfolio.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Portfolio {
+// Member Variable
+    private ArrayList<Project> projects;
+
+// Constructor
+    public Portfolio() {
+        this.projects = new ArrayList<Project>();
+    }
+
+// Member Methods
+public void addProjectToPortfolio(Project project) {
+    projects.add(project);
+}
+
+    public double getPortfolioCost() {
+        double total = 0;
+        for (Project project : projects) {
+            total += project.getInitialCost();
+        }
+        return total;
+    }
+
+    public void showPortfolio() {
+        System.out.println("All Projects in Portfolio:");
+        for (Project project : projects) {
+            System.out.println(project.elevatorPitch());
+        }
+        // System.out.printf("Total Cost: %f", this.getPortfolioCost());
+    }
+
+// Getter that shows all the projects that are contained in the portfolio
+    public String getPortfolio() {
+        return this.projects.toString();
+    }
+
+}
