f93d1f296e8455e949e50eed0f6c6378b7c89684
samuel hughes <samuelhugh33@gmail.com>
2022-11-10 00:59:40 -0600

    Refactoring

--- 02-java-oop/04-zoo-keeper-pt2/GiantBatsTest.java
+++ 02-java-oop/04-zoo-keeper-pt2/GiantBatsTest.java
@@ -1,16 +1,17 @@
-// Set-up My Test File For My GiantBats Objects/Instances
+// Defining Test File for GiantBats Objects/Instances Testing
 public class GiantBatsTest {
     // Entry Point Method
     public static void main(String[] args) {
-        // Creating A GiantBats Object
+        // Creating A GiantBats Instance
         GiantBats bat = new GiantBats();
         bat.attackTown();
-        bat.attackTown();
-        bat.attackTown();
-        bat.eatHumans();
-        bat.eatHumans();
-        bat.fly();
-        bat.fly();
-        System.out.println("First GiantBats Energy: " + bat.displayEnergy());
+        // bat.attackTown();
+        // bat.attackTown();
+        // bat.eatHumans();
+        // bat.eatHumans();
+        // bat.fly();
+        // bat.fly();
+        // System.out.println("First GiantBats Energy: " + bat.displayEnergy());
     }
+
 }
