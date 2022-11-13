13645a0942dd1e8015128d0ddd9bad5b84fa6fda
samuel hughes <samuelhugh33@gmail.com>
2022-11-12 18:40:15 -0600

    Refactoring

--- 02-java-oop/01-project-class/Portfolio.java
+++ 02-java-oop/01-project-class/Portfolio.java
@@ -1,3 +1,4 @@
+// Importing the Imports I need
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -6,34 +7,34 @@ public class Portfolio {
     private ArrayList<Project> projects;
 
 // Constructor
-    public Portfolio() {
-        this.projects = new ArrayList<Project>();
-    }
+public Portfolio() {
+    this.projects = new ArrayList<Project>();
+}
 
 // Member Methods
 public void addProjectToPortfolio(Project project) {
     projects.add(project);
 }
 
-    public double getPortfolioCost() {
-        double total = 0;
-        for (Project project : projects) {
-            total += project.getInitialCost();
-        }
-        return total;
+public double getPortfolioCost() {
+    double total = 0;
+    for (Project project : projects) {
+        total += project.getInitialCost();
     }
+    return total;
+}
 
-    public void showPortfolio() {
-        System.out.println("All Projects in Portfolio:");
-        for (Project project : projects) {
-            System.out.println(project.elevatorPitch());
-        }
-        // System.out.printf("Total Cost: %f", this.getPortfolioCost());
+public void showPortfolio() {
+    System.out.println("All Projects in Portfolio:");
+    for (Project project : projects) {
+        System.out.println(project.elevatorPitch());
     }
+    // System.out.printf("Total Cost: %f", this.getPortfolioCost());
+}
 
-// Getter that shows all the projects that are contained in the portfolio
-    public String getPortfolio() {
-        return this.projects.toString();
-    }
+// Getter Instance Method that shows all the projects that are contained in a portfolio
+public String getPortfolio() {
+    return this.projects.toString();
+}
 
 }
