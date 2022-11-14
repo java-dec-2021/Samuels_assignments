95584eb09433dbdbbf661e8c9dc8ed365597e353
samuel hughes <samuelhugh33@gmail.com>
2022-11-13 19:29:03 -0600

    Refactoring

--- 02-java-oop/04-zoo-keeper-pt2/Mammals.java
+++ 02-java-oop/04-zoo-keeper-pt2/Mammals.java
@@ -1,20 +1,20 @@
 // Define Class With Verb Name
 public class Mammals {
 
-    // Setting Attributes/Column
+    // Setting Attribute/Column
     public int energyLevel;
 
     // Setting Constructor Method
     public Mammals(int energyLevel) {
-        // MUST use "THIS" when variable names are the same
-        // Same As "SELF" From Python
+        // MUST use "this" when Variable names are the same as Member Variables
+        // "this" same as "self" from Python
         this.energyLevel = energyLevel;
     }
 
     // Setting My Methods/Functionality/Behavior
     public int displayEnergy() {
         // Returning the current energyLevel for "this" Class
-        // Same As "SELF" From Python
+        // Same as "self" from Python
         return this.energyLevel;
     }
 
