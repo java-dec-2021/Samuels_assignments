30c4714cb5c2ce0826811f9749306ce2695bbaea
samuel hughes <samuelhugh33@gmail.com>
2022-11-13 19:30:59 -0600

    Refactoring

--- 02-java-oop/05-object-master-pt1/Humans.java
+++ 02-java-oop/05-object-master-pt1/Humans.java
@@ -1,10 +1,10 @@
 public class Humans {
-// Attributes, since I will be interacting with objects of the same type I want the children of this superclass to be able to have access to their own attributes
+// Member Variables or Attributes
     private int health;
     private int strength;
     private int stealth;
     private int intelligence;
-// Constructors with default values do not need to take have parameters
+// Constructors with default values do not need to have parameters
     public Humans() {
         this.health=100;
         this.strength=3;
@@ -14,9 +14,9 @@ public class Humans {
 // public Humans() {
     //this(100, 3, 3, 3);
     // }
-// Methods
+// Instance Methods
     public void attack(Humans otherHuman) {
-        // Now the "otherHuman" object can access its own attributes
+        // Now the "otherHuman" object can access its own attributes via Dereferencing
         otherHuman.health-=this.strength;
         System.out.println("This human attacked another human by " + this.strength + " The other humans health is now " + otherHuman.health);
     }
