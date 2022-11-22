06c206cfd1d279bf2ef845e8cda3a24fad5b7ccf
samuel hughes <samuelhugh33@gmail.com>
2022-11-21 22:15:28 -0600

    Refactoring

---
+++ 02-java-oop/08-pokemon-optional/Pokedex.java
@@ -0,0 +1,14 @@
+// Importing the Imports I need
+import java.util.ArrayList;
+// Defining the Class with Verb name, Extending the Abstract Class (Figure out why this class needs to be abstract)
+public abstract class Pokedex extends AbstractPokemon {
+// Member Variable or Attribute
+    public ArrayList<String> myPokemons;
+// Implementing "listPokemon" Member Method from Abstract Class
+public void listPokemon() {
+    for(String poke : myPokemons) {
+        System.out.println(poke);
+    }
+}
+
+}
