96b15f589a6ffa5d2f24f79552484aa4eff98bc5
samuel hughes <samuelhugh33@gmail.com>
2022-11-23 22:12:36 -0600

    Refactoring

--- 02-java-oop/09-phone/Phone.java
+++ 02-java-oop/09-phone/Phone.java
@@ -1,13 +1,13 @@
-// Set-up My Abstract class
+// Defining the Abstract Class, My base Class that closely related Regular Class will be Inheriting
 public abstract class Phone {
 
-    // Setting Some Attributes
+// Member Variables, These Fields are not "static" and "final" by default, Therefore Fields cannot be Constant in Abstract Classes
     private String versionNumber;
     private int batteryPercentage;
     private String carrier;
     private String ringTone;
 
-    // Can Use Constructor Method To SET Default Values For My Attributes (i.e. this.field = 0, or this.fieldName = fieldName, or field++)
+// The Constructor Method can SET Default Values for the Attributes, (e.g. this.field = 0, or this.fieldName = fieldName, or field++), Also with Abstract Classes Member Methods; They are allowed to have all three levels of Access Modifiers
     public Phone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
         this.versionNumber = versionNumber;
         this.batteryPercentage = batteryPercentage;
@@ -15,11 +15,10 @@ public abstract class Phone {
         this.ringTone = ringTone;
     }
 
-    // abstract method. This method will be implemented by the subclasses
+// Abstract Method for Abstract Classes. This Method will be Implemented by the Subclasses, Also since this an Explicit Abstract Method Using the "abstract" Keyword; This Class must now be Abstract itself
     public abstract void displayInfo();
 
-    // Getters
-
+// Getters
     // Getter For version number
     public String getVersionNumber() {
         return this.versionNumber;
@@ -39,4 +38,5 @@ public abstract class Phone {
     public String getRingTone() {
         return this.ringTone;
     }
+
 }
