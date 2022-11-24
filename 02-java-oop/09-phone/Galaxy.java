b569ce062ce5c887d23f6c256178decf2a4576bd
samuel hughes <samuelhugh33@gmail.com>
2022-11-23 22:10:34 -0600

    Refactoring

--- 02-java-oop/09-phone/Galaxy.java
+++ 02-java-oop/09-phone/Galaxy.java
@@ -1,24 +1,22 @@
 public class Galaxy extends Phone implements Ringable {
-
+// Constructor
     public Galaxy(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
         super(versionNumber, batteryPercentage, carrier, ringTone);
     }
-
+// Instance Methods being Implemented from the Interface and Abstract Classes Respectively 
     @Override
     public String ring() {
-        // your code here
         return "Galaxy " + this.getVersionNumber() + " Says " + this.getRingTone();
     }
 
     @Override
     public String unlock() {
-        // your code here
         return "Unlocking Via Finger Print";
     }
 
     @Override
     public void displayInfo() {
-        // your code here
         System.out.println("Galaxy " + this.getVersionNumber() + " From " + this.getCarrier());
     }
+
 }
