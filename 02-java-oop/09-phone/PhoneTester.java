e15c486cfd3f59d6a6e107c46d3998f595dc3e0d
samuel hughes <samuelhugh33@gmail.com>
2022-11-24 23:57:16 -0600

    Refactoring

--- 02-java-oop/09-phone/PhoneTester.java
+++ 02-java-oop/09-phone/PhoneTester.java
@@ -1,18 +1,19 @@
-// Set-up Test File class
+// Defining Test File class
 public class PhoneTester {
-    //Entry Point
+    // Entry Point
     public static void main(String[] args) {
         Galaxy s9 = new Galaxy("s9", 99, "Verizon", "Ring Ring Ring!");
         IPhone iphoneTen = new IPhone("X", 100, "AT&T", "Zing");
 
-        // Galaxy Test
+        // Galaxy Member Methods Implementation Test
         s9.displayInfo();
         System.out.println(s9.ring());
         System.out.println(s9.unlock());
 
-        // IPhone Test
+        // IPhone Member Methods Implementation Test
         iphoneTen.displayInfo();
         System.out.println(iphoneTen.ring());
         System.out.println(iphoneTen.unlock());
+
     }
 }
