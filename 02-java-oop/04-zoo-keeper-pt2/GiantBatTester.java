7d3bfa9e3088c02f0bb2da69d41baa1a30c29fef
samuel hughes <samuelhugh33@gmail.com>
2022-12-09 14:59:59 -0600

    Refactoring

---
+++ 02-java-oop/04-zoo-keeper-pt2/GiantBatTester.java
@@ -0,0 +1,17 @@
+// Defining Test File for GiantBat Objects/Instances Testing
+public class GiantBatTester {
+    // Entry Point Method
+    public static void main(String[] args) {
+        // Creating A GiantBat Instance
+        GiantBat bat = new GiantBat();
+        bat.attackTown();
+        // bat.attackTown();
+        // bat.attackTown();
+        // bat.eatHumans();
+        // bat.eatHumans();
+        // bat.fly();
+        // bat.fly();
+        // System.out.println("First GiantBat Energy: " + bat.displayEnergy());
+    }
+
+}
