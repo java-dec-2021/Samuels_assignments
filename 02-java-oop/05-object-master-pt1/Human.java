9c56ffcc578c5a9c1f69b22d481c53f0abf0178d
samuel hughes <samuelhugh33@gmail.com>
2022-12-10 23:26:06 -0600

    Refactoring

---
+++ 02-java-oop/05-object-master-pt1/Human.java
@@ -0,0 +1,24 @@
+public class Human {
+// Member Variables or Attributes
+    private int health;
+    private int strength;
+    private int stealth;
+    private int intelligence;
+// Constructors with default values do not need to have parameters
+    public Human() {
+        this.health=100;
+        this.strength=3;
+        this.stealth=3;
+        this.intelligence=3;
+    }
+// public Humans() {
+    //this(100, 3, 3, 3);
+    // }
+// Instance Methods
+    public void attack(Human otherHuman) {
+        // Now the "otherHuman" object can access its own attributes via Dereferencing
+        otherHuman.health-=this.strength;
+        System.out.println("This human attacked another human by " + this.strength + " The other humans health is now " + otherHuman.health);
+    }
+
+}
