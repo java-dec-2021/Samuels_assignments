490a8a17542e943c0ea4652bc9b770629d5c6e8a
samuel hughes <samuelhugh33@gmail.com>
2022-12-12 01:27:20 -0600

    Refactoring

--- 02-java-oop/05-object-master-pt1/HumanTest.java
+++
@@ -1,8 +0,0 @@
-public class HumanTest {
-    public static void main(String[] args) {
-        Humans human1 = new Humans();
-        Humans human2 = new Humans();
-        human1.attack(human2);
-    }
-
-}
