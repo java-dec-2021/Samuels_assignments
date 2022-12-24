edbab260d2fe24b4a2383c356cc7e5a0894c728b
samuel hughes <samuelhugh33@gmail.com>
2022-12-23 21:51:06 -0600

    Refactoring

--- 02-java-oop/08-pokemon-optional/AbstractPokemon.java
+++ 02-java-oop/08-pokemon-optional/AbstractPokemon.java
@@ -1,12 +1,12 @@
-// Defining the Abstract Class, MUST Explicitly use "abstract" Keyword
+// Defining the Abstract Class, Must have Noun name with ALL Classes, MUST Explicitly use "abstract" Keyword
 public abstract class AbstractPokemon implements PokemonInterface {
 // Giving the "createPokemon" Method Definition
-public Pokemons createPokemons(String name, int health, String type) {
-    Pokemons poke = new Pokemons(name, health, type);
+public Pokemon createPokemon(String name, int health, String type) {
+    Pokemon poke = new Pokemon(name, health, type);
     return poke;
 }
 // Giving the "pokemonInfo" Method Definition
-public String pokemonInfo(Pokemons pokemon) {
+public String pokemonInfo(Pokemon pokemon) {
     return pokemon.toString();
 }
 }
