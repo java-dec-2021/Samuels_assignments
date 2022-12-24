301876dc844c0359fc27b4e0bd463080d9867970
samuel hughes <samuelhugh33@gmail.com>
2022-12-23 21:52:40 -0600

    Refactoring

--- 02-java-oop/08-pokemon-optional/Pokedex.java
+++ 02-java-oop/08-pokemon-optional/Pokedex.java
@@ -1,12 +1,12 @@
 // Importing the Imports I need
 import java.util.ArrayList;
-// Defining the Class with Verb name, Extending the Abstract Class (Figure out why this class needs to be abstract)
+// Defining the Class with Noun name, Extending the Abstract Class (Figure out why this class needs to be abstract), Does this Abstract Class need a Constructor?
 public abstract class Pokedex extends AbstractPokemon {
 // Member Variable or Attribute
-    public ArrayList<String> myPokemons;
+    public ArrayList<String> myPokemon;
 // Implementing "listPokemon" Member Method from Abstract Class
 public void listPokemon() {
-    for(String poke : myPokemons) {
+    for(String poke : myPokemon) {
         System.out.println(poke);
     }
 
