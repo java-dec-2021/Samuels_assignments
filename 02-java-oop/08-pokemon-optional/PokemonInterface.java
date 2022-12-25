3117d36b4cf83e5293decbf5fd24b526fbdbc8b0
samuel hughes <samuelhugh33@gmail.com>
2022-12-24 20:29:42 -0600

    Refactoring

--- 02-java-oop/08-pokemon-optional/PokemonInterface.java
+++ 02-java-oop/08-pokemon-optional/PokemonInterface.java
@@ -2,9 +2,9 @@
 public interface PokemonInterface {
 // Interfaces' Definition and/or Member Methods, Which all MUST be Implement and Specified if a Class is to Implement any Interface - Unless the Methods are "default" and "static" or if an Abstract Class is Implementing the Interface, Then the Subclass Extending the Abstract Class MUST Implement the rest of the Methods
 // Abstract Method to create a Pokemon
-Pokemons createPokemon(String name, int health, String type);
+Pokemon createPokemon(String name, int health, String type);
 // Default "Abstract" Method for Interfaces to Display a Pokemon's name, health, and type
-String pokemonInfo(Pokemons pokemon);
+String pokemonInfo(Pokemon pokemon);
 // Default "Abstract" Method for Interfaces to list all the Pokemon in the Pokedex
 void listPokemon();
 }
