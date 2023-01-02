63122b13d72a2c00da15d6b508f6d6482c8a5256
samuel hughes <samuelhugh33@gmail.com>
2023-01-01 21:37:59 -0600

    Refactoring

---
+++ 02-java-oop/10-calculator-i/BasicCalculatorTester.java
@@ -0,0 +1,14 @@
+// Defining Test File Class with Noun name
+public class BasicCalculatorTester {
+    // Entry Point Method
+    public static void main(String[] args) {
+        // Off of the BasicCalculator JavaBean I am Instantiating an Object
+        BasicCalculator calculator = new BasicCalculator();
+        // Testing the Member Methods, Getter, and Setters
+        calculator.setOperandOne(55.5);
+        calculator.setOperandTwo(2.5);
+        calculator.setOperand("-");
+        System.out.println(calculator.performOperation());
+        System.out.println(calculator.getResult());
+    }
+}
